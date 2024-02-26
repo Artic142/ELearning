124d8b77f3f6a295066b546cb08ee733df94f0c9
Artic142 <artichandore7@gmail.com>
2024-02-26 15:02:49 +0530

    con

--- dbConnection.php
+++ dbConnection.php
@@ -1,7 +1,7 @@
 <?php
 $db_host = "artiserver.mysql.database.azure.com";
 $db_user = "root";
-$db_password = "";
+$db_password = "Arti@142";
 $db_name = "lms_db";
 
 $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
